a0b692b84ee576b012791db81670ca0ad4bda24f
Sayan Chatterjee <sayanc354@gmail.com>
2024-12-07 05:32:07 +0530

    uplo

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def download_file(file_name):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5678, debug=True)
